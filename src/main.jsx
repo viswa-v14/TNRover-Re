82b17a646cfa6339ef784195f0ae46927af7f400
ViswaVeeramuthu <viswav.20cse@kongu.edu>
2023-12-16 22:24:40 +0530

    check1

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+// import App from "./App";
+import HomePage from "./pages/HomePage";
 
-createApp(App).use(BrowserRouter).mount('#app');
+// createApp(App).use(BrowserRouter).mount('#app');
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <HomePage />
   </React.StrictMode>
 );
