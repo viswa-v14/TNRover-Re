f54d64e55e88529cac859d9007f49853dd5e26ce
ViswaVeeramuthu <viswav.20cse@kongu.edu>
2023-12-16 22:27:48 +0530

    check2

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,20 @@ import HomePage from "./pages/HomePage";
 import CategoryPage from "./pages/CategoryPage";
 import ListPage from "./pages/ListPage";
 import PropertyPage from "./pages/PropertyPage";
+import List from "./components/List/List";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      {/* <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<HomePage />}/>
           <Route path="/category" element={<CategoryPage />}/>
           <Route path="/list" element={<ListPage />}/>
           <Route path="/property" element={<PropertyPage />} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
+      <List/>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import App from "./App";
-import HomePage from "./pages/HomePage";
+import App from "./App";
 
-// createApp(App).use(BrowserRouter).mount('#app');
+createApp(App).use(BrowserRouter).mount('#app');
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HomePage />
+    <App />
   </React.StrictMode>
 );
