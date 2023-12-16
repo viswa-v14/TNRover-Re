f6a75011abb450d461751637b337b03edf356aa2
ViswaVeeramuthu <viswav.20cse@kongu.edu>
2023-12-16 22:42:16 +0530

    check5

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,18 @@ import HomePage from "./pages/HomePage";
 import CategoryPage from "./pages/CategoryPage";
 import ListPage from "./pages/ListPage";
 import PropertyPage from "./pages/PropertyPage";
-import List from "./components/List/List";
 
 function App() {
   return (
     <div className="App">
-      {/* <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomePage />}/>
+          <Route path="/TNRover-Re" element={<HomePage />}/>
           <Route path="/category" element={<CategoryPage />}/>
           <Route path="/list" element={<ListPage />}/>
           <Route path="/property" element={<PropertyPage />} />
         </Routes>
-      </BrowserRouter> */}
-      <h1>Test</h1>
-      {console.log("checking")}
+      </BrowserRouter>
     </div>
   );
 }
