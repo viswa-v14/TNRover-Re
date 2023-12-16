f23100d7cd2f658857f75ec47a0fa34726081f4f
ViswaVeeramuthu <viswav.20cse@kongu.edu>
2023-12-16 21:43:08 +0530

    deploy1

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import PropertyPage from "./pages/PropertyPage";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<HomePage />}/>
           <Route path="/category" element={<CategoryPage />}/>
