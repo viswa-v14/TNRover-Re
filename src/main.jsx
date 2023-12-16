12e4ef62749b65de5e0a125e8324afbdaca46b94
ViswaVeeramuthu <viswav.20cse@kongu.edu>
2023-12-16 22:21:58 +0530

    deploy6

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+createApp(App).use(BrowserRouter).mount('#app');
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
